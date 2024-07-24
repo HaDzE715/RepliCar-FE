1d3094641607d55cbe4da58206e501e97ef37f02
HaDzE7 <hadebayaa@gmail.com>
2024-07-24 15:24:13 +0300

    Added Loading Skeleton

--- src/Components/Product.js
+++ src/Components/Product.js
@@ -1,13 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import Skeleton from "@mui/material/Skeleton";
 import "../Style/Product.css";
 
 const Product = ({ id, name, size, price, image }) => {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
   return (
     <Link to={`/product/${id}`} className="product-link">
       <div className="product-card">
         <div className="product-image-container">
-          <img src={image} alt={name} className="product-image" />
+          {!imageLoaded && (
+            <Skeleton
+              variant="rectangular"
+              width="100%"
+              height={255}
+              className="image-skeleton"
+            />
+          )}
+          <img
+            src={image}
+            alt={name}
+            className={`product-image ${imageLoaded ? "loaded" : "loading"}`}
+            onLoad={() => setImageLoaded(true)}
+            style={{ display: imageLoaded ? "block" : "none" }}
+          />
         </div>
         <div className="product-details">
           <h2 className="product-name">{name}</h2>
