f78dc92b96a8e90da8f49934a90ddbb26dc8c3d5
HaDzE7 <hadebayaa@gmail.com>
2024-07-30 01:16:12 +0300

    Fixed DropDown bug

--- src/Pages/CheckOutPage.js
+++ src/Pages/CheckOutPage.js
@@ -1,10 +1,32 @@
-import React, { useState } from "react";
+import * as React from "react";
+import { useState, useEffect, useRef } from "react";
 import { useCart } from "../Components/CartContext";
-import "../Style/CheckoutPage.css";
+import { prefixer } from "stylis";
+import rtlPlugin from "stylis-plugin-rtl";
+import { CacheProvider } from "@emotion/react";
+import createCache from "@emotion/cache";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import axios from "axios";
+import "../Style/CheckoutPage.css"; // Ensure this import is correct
 
-const CheckoutPage = () => {
+const rtlCache = createCache({
+  key: "muirtl",
+  stylisPlugins: [prefixer, rtlPlugin],
+});
+
+const ltrCache = createCache({
+  key: "mui",
+});
+
+export default function CheckoutPage() {
   const { cart, dispatch } = useCart();
   const [isCartVisible, setIsCartVisible] = useState(false);
+  const dropdownRef = useRef(null); // Reference for the dropdown
+  const buttonRef = useRef(null); // Reference for the button
+  const [rtl] = useState(true); // Set initial state to true for Hebrew RTL
 
   const calculateTotalQuantity = () => {
     return cart.reduce((total, item) => total + item.quantity, 0);
@@ -38,79 +60,323 @@ const CheckoutPage = () => {
     dispatch({ type: "DECREASE_QUANTITY", id });
   };
 
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = {
+      firstName: formData.get("firstName"),
+      lastName: formData.get("lastName"),
+      phone: formData.get("phone"),
+      email: formData.get("email"),
+      city: formData.get("city"),
+      streetAddress: formData.get("streetAddress"),
+      clientType: formData.get("clientType"),
+      orderNotes: formData.get("orderNotes"), // Add this line
+    };
+
+    try {
+      const response = await axios.post(
+        `${process.env.REACT_APP_API_URL}/clientInfo`,
+        data
+      ); // Replace with your backend URL
+      if (response.status === 200) {
+        alert("Client information submitted successfully");
+      } else {
+        alert("Failed to submit client information");
+      }
+    } catch (error) {
+      console.error("Error submitting client information:", error);
+      alert("An error occurred while submitting the client information");
+    }
+  };
+
+  useEffect(() => {
+    // Close dropdown if clicking outside of it
+    const handleClickOutside = (event) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target)
+      ) {
+        setIsCartVisible(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (isCartVisible) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isCartVisible]);
+
   return (
     <div className="checkout-page">
       <div className={`checkout-header ${isCartVisible ? "expanded" : ""}`}>
         <div className="checkout-summary">
           <span className="product-quantity">
-            כמות מוצרים - {calculateTotalQuantity()}
+            סך הכל: {calculateTotalQuantity()}
           </span>
-          <span className="total-price">סך הכל - {calculateTotalPrice()}₪</span>
+          <span className="total-price">סך הכל: {calculateTotalPrice()}₪</span>
         </div>
-        <button onClick={toggleCartVisibility} className="view-cart-link">
+        <button
+          ref={buttonRef}
+          onClick={toggleCartVisibility}
+          className="view-cart-link"
+        >
           לחץ לצפייה בעגלה{" "}
           <span className={`arrow ${isCartVisible ? "up" : "down"}`}></span>
         </button>
       </div>
-      {isCartVisible && (
-        <div className="checkout-cart-dropdown">
-          <div className="checkout-cart-items">
-            {cart.length > 0 ? (
-              cart.map((item) => (
-                <div key={item._id} className="checkout-cart-item">
-                  <img src={item.image} alt={item.name} />
-                  <div className="checkout-cart-item-details">
-                    <h3>{item.name}</h3>
-                    <p>כמות: {item.quantity}</p>
-                    <p>מחיר: {item.price}₪</p>
-                    <div className="checkout-cart-action-section">
-                      <div className="checkout-cart-quantity-section">
+      <div style={{ position: "relative" }}>
+        {isCartVisible && (
+          <div ref={dropdownRef} className="checkout-cart-dropdown">
+            <div className="checkout-cart-items">
+              {cart.length > 0 ? (
+                cart.map((item) => (
+                  <div key={item._id} className="checkout-cart-item">
+                    <img src={item.image} alt={item.name} />
+                    <div className="checkout-cart-item-details">
+                      <h3>{item.name}</h3>
+                      <p>כמות: {item.quantity}</p>
+                      <p>מחיר: {item.price}₪</p>
+                      <div className="checkout-cart-action-section">
+                        <div className="checkout-cart-quantity-section">
+                          <button
+                            onClick={() => handleIncreaseQuantity(item._id)}
+                          >
+                            +
+                          </button>
+                          <span>{item.quantity}</span>
+                          <button
+                            onClick={() => handleDecreaseQuantity(item._id)}
+                          >
+                            -
+                          </button>
+                        </div>
                         <button
-                          onClick={() => handleIncreaseQuantity(item._id)}
+                          className="checkout-cart-remove-button"
+                          onClick={() => handleRemoveFromCart(item._id)}
                         >
-                          +
-                        </button>
-                        <span>{item.quantity}</span>
-                        <button
-                          onClick={() => handleDecreaseQuantity(item._id)}
-                        >
-                          -
+                          מחק
                         </button>
                       </div>
-                      <button
-                        className="checkout-cart-remove-button"
-                        onClick={() => handleRemoveFromCart(item._id)}
-                      >
-                        מחק
-                      </button>
                     </div>
                   </div>
-                </div>
-              ))
-            ) : (
-              <p style={{ fontFamily: "Noto Sans Hebrew" }}>אין מוצרים בעגלה</p>
-            )}
-          </div>
-          <div className="checkout-cart-totals">
-            <div className="totals-row">
-              <span>סכום ביניים:</span>
-              <span>{calculateSubtotal()}₪</span>
+                ))
+              ) : (
+                <p>אין מוצרים בעגלה</p>
+              )}
             </div>
-            <div className="totals-row">
-              <span>משלוח:</span>
-              <span>{calculateShipping()}₪</span>
-            </div>
-            <div className="totals-row">
-              <span style={{ fontWeight: "600" }}>סך הכל:</span>
-              <span style={{ fontWeight: "600" }}>
-                {calculateTotalPrice()}₪
-              </span>
+            <div className="checkout-cart-totals">
+              <div className="totals-row">
+                <span>סכום ביניים:</span>
+                <span>{calculateSubtotal()}₪</span>
+              </div>
+              <div className="totals-row">
+                <span>משלוח:</span>
+                <span>{calculateShipping()}₪</span>
+              </div>
+              <div className="totals-row">
+                <span>סך הכל:</span>
+                <span>{calculateTotalPrice()}₪</span>
+              </div>
             </div>
           </div>
+        )}
+        {/* Form */}
+        <div className="form-container-checkout">
+          <CacheProvider value={rtl ? rtlCache : ltrCache}>
+            <Box
+              sx={{
+                width: "100%",
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                my: 4, // Margin from top and bottom
+                mx: 2, // Margin from sides
+              }}
+            >
+              <Typography
+                variant="h4"
+                component="h1"
+                sx={{ mb: 2, fontFamily: "Noto Sans Hebrew" }}
+              >
+                פרטי לקוח
+              </Typography>
+              <Box
+                component="form"
+                onSubmit={handleSubmit}
+                sx={{
+                  flexGrow: 1,
+                  display: "flex",
+                  flexDirection: "column",
+                  gap: 2,
+                  width: "100%",
+                  maxWidth: "500px",
+                }}
+                dir={rtl ? "rtl" : ""}
+              >
+                <Box
+                  sx={{
+                    display: "flex",
+                    gap: 2,
+                    width: "100%",
+                  }}
+                >
+                  <TextField
+                    name="firstName"
+                    label="שם פרטי"
+                    variant="standard"
+                    fullWidth
+                    required
+                    sx={{
+                      mb: 2,
+                      "& .MuiInputBase-input": {
+                        fontFamily: "Noto Sans Hebrew",
+                      },
+                    }}
+                    InputLabelProps={{
+                      className: rtl ? "rtl-asterisk" : "",
+                      sx: { fontFamily: "Noto Sans Hebrew" },
+                    }}
+                  />
+                  <TextField
+                    name="lastName"
+                    label="שם משפחה"
+                    variant="standard"
+                    fullWidth
+                    required
+                    sx={{
+                      mb: 2,
+                      "& .MuiInputBase-input": {
+                        fontFamily: "Noto Sans Hebrew",
+                      },
+                    }}
+                    InputLabelProps={{
+                      className: rtl ? "rtl-asterisk" : "",
+                      sx: { fontFamily: "Noto Sans Hebrew" },
+                    }}
+                  />
+                </Box>
+                <TextField
+                  name="phone"
+                  label="טלפון"
+                  type="tel"
+                  variant="standard"
+                  fullWidth
+                  required
+                  sx={{
+                    mb: 2,
+                    "& .MuiInputBase-input": { fontFamily: "Noto Sans Hebrew" },
+                  }}
+                  InputLabelProps={{
+                    className: rtl ? "rtl-asterisk" : "",
+                    sx: { fontFamily: "Noto Sans Hebrew" },
+                  }}
+                />
+                <TextField
+                  name="email"
+                  label='כתובת דוא"ל'
+                  type="email"
+                  variant="standard"
+                  fullWidth
+                  required
+                  sx={{
+                    mb: 2,
+                    "& .MuiInputBase-input": { fontFamily: "Noto Sans Hebrew" },
+                  }}
+                  InputLabelProps={{
+                    className: rtl ? "rtl-asterisk" : "",
+                    sx: { fontFamily: "Noto Sans Hebrew" },
+                  }}
+                />
+                <Box
+                  sx={{
+                    display: "flex",
+                    gap: 2,
+                    width: "100%",
+                  }}
+                >
+                  <TextField
+                    name="city"
+                    label="עיר"
+                    variant="standard"
+                    fullWidth
+                    required
+                    sx={{
+                      mb: 2,
+                      "& .MuiInputBase-input": {
+                        fontFamily: "Noto Sans Hebrew",
+                      },
+                    }}
+                    InputLabelProps={{
+                      className: rtl ? "rtl-asterisk" : "",
+                      sx: { fontFamily: "Noto Sans Hebrew" },
+                    }}
+                  />
+                  <TextField
+                    name="streetAddress"
+                    label="כתובת רחוב"
+                    variant="standard"
+                    fullWidth
+                    required
+                    sx={{
+                      mb: 2,
+                      "& .MuiInputBase-input": {
+                        fontFamily: "Noto Sans Hebrew",
+                      },
+                    }}
+                    InputLabelProps={{
+                      className: rtl ? "rtl-asterisk" : "",
+                      sx: { fontFamily: "Noto Sans Hebrew" },
+                    }}
+                  />
+                </Box>
+                <TextField
+                  name="orderNotes"
+                  label="הערות להזמנה/ לשליח"
+                  variant="outlined"
+                  multiline
+                  rows={4}
+                  fullWidth
+                  sx={{
+                    mb: 2,
+                    "& .MuiInputBase-input": {
+                      fontFamily: "Noto Sans Hebrew",
+                    },
+                  }}
+                  InputLabelProps={{
+                    className: rtl ? "rtl-asterisk" : "",
+                    sx: { fontFamily: "Noto Sans Hebrew" },
+                  }}
+                />
+                <Button
+                  type="submit"
+                  variant="contained"
+                  sx={{
+                    backgroundColor: "black", // Set the background color to black
+                    color: "white", // Set the text color to white
+                    fontFamily: "Noto Sans Hebrew", // Set the font family
+                    "&:hover": {
+                      backgroundColor: "darkgrey", // Optional: Set a different color on hover
+                    },
+                  }}
+                >
+                  שלח
+                </Button>
+              </Box>
+            </Box>
+          </CacheProvider>
         </div>
-      )}
+      </div>
     </div>
   );
-};
-
-export default CheckoutPage;
+}
